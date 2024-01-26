b7a8f4de656da7f684a9d1ec8c8bba94757d3864
hayeon <linaad9809@gmail.com>
2024-01-26 13:56:34 +0900

    rownun rank view quiz commit

---
+++ sqlscript/16_ROWNUM.sql
@@ -0,0 +1,76 @@
+/*
+ * 		오라클 RWONUM 컬럼 : 오라클이 SELECT 결과에 부여하는 로우(행)번호
+ * 						  오라클이 내부적으로 관리를 하기 위한 컬럼
+ * */
+
+-- 1) 상품테이블 상품 가격이 높은 순으로 정렬해 조회
+SELECT *
+FROM TBL_PRODUCT 
+ORDER BY PRICE DESC;
+
+-- 2) ROWNUM 컬럼을 출력해봅니다
+SELECT ROWNUM, TP.*
+FROM TBL_PRODUCT TP 
+ORDER BY PRICE DESC;
+
+
+-- 3) 서브쿼리로 ROWNUM 순서 추가
+SELECT ROWNUM, TP.*
+FROM (
+	SELECT *
+	FROM TBL_PRODUCT 
+	ORDER BY PRICE DESC
+) TP;
+
+
+-- 4) 위의 결과에 특정 ROWNUM만 보여주기  
+SELECT ROWNUM, TP.*
+FROM (
+	SELECT *
+	FROM TBL_PRODUCT 
+	ORDER BY PRICE DESC
+) TP
+--WHERE ROWNUM = 1;				
+WHERE ROWNUM IS NOT NULL;		-- 가능한 조건식은 ROWNUM이 1이 포함되는 경우만 가능
+
+
+
+-- 5) 중간 일부를 가져오기 위해서 BETWEEN AND 연산 사용. 위 결과에 서브쿼리(TABLE)를 사용
+-- 게시판 글목록 페이지 번호에 사용됨
+SELECT *
+FROM
+	(
+		SELECT ROWNUM AS R, TP.*		
+		FROM (
+			SELECT *
+			FROM TBL_PRODUCT 
+			ORDER BY PRICE DESC
+		) TP		
+	)
+WHERE R BETWEEN 1 AND 10;
+
+
+-- 6) 오라클 12C 버전부터는 FETCH 명령이 있습니다.	ROWNUM <= 2 와 동일한 결과
+SELECT TP.*
+FROM TBL_PRODUCT  TP
+ORDER BY PRICE DESC 
+FETCH FIRST 2 ROWS ONLY;	--TOP 2(N) 가져오기
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sqlscript/17_RANK.sql
@@ -0,0 +1,108 @@
+/*
+ * 	오라클 RANK 함수
+ * 	예제 : 고객 - 상품별 최대구매금액과 해당 상품코드를 보여주기
+ * 		ㄴ 고객ID 로 1차 그룹화, 고객 내에서 상품코드로 2차 그룹화
+ * */
+
+
+-- 1) GROUP BY
+SELECT 	TB.CUSTOMID, 
+		TB.PCODE,
+		SUM(TP.PRICE * TB.QUANTITY) AS SUM -- 가격 수량에 대한 그룹화 함수 SUM 실행
+FROM 
+		TBL_BUY TB, TBL_PRODUCT TP
+WHERE 	
+		TB.PCODE = TP.PCODE 
+GROUP BY 
+		TB.CUSTOMID, TB.PCODE;
+		
+	
+INSERT INTO TBL_BUY  VALUES (BUY_PK_SEQ.NEXTVAL, 'mina012', 'JINRMn5', 13, '2024-01-26');
+
+
+SELECT *
+FROM TBL_BUY;
+
+
+-- 1)
+SELECT tb.CUSTOMID , tb.pcode,
+		sum(tp.price * tb.quantity) 
+FROM 
+	TBL_BUY tb, 
+	TBL_PRODUCT tp
+WHERE tb.pcode = tp.pcode
+GROUP BY tb.customid, tb.pcode;
+
+
+-- 2) 정렬 
+SELECT tb.CUSTOMID, tb.pcode,
+		sum(tp.price * tb.quantity) AS pay_sum
+FROM 
+	TBL_BUY tb, 
+	TBL_PRODUCT tp
+WHERE tb.pcode = tp.pcode
+GROUP BY tb.customid, tb.pcode
+ORDER BY 
+	tb.CUSTOMID, pay_sum desc;
+	
+	
+
+-- 3) 위 결과에 rank 함수를 사용해 순위를 보여주세요
+SELECT	  tb.CUSTOMID 
+		, tb.pcode
+		, sum(tp.price * tb.quantity) 
+		, RANK() 
+				over(PARTITION BY tb.customid
+					ORDER BY sum(tp.price * tb.quantity) DESC) AS RANK	-- OVER () 안에는 순위를 매기기 위해 어떤 컬럼으로 정렬 할지 사용 
+FROM 
+	TBL_BUY tb, 
+	TBL_PRODUCT tp
+WHERE tb.pcode = tp.pcode
+GROUP BY tb.customid, tb.pcode;
+	
+-- 4) 위 결과에 서브쿼리를 사용해 rank = 1 인 조건을 보여주세요
+WITH customSale
+AS 
+(
+	SELECT	  tb.CUSTOMID 
+			, tb.pcode
+			, SUM(tp.price * tb.quantity) 
+			, RANK() 
+					over(PARTITION BY tb.customid
+						 ORDER BY sum(tp.price * tb.quantity) DESC) AS RANK	-- OVER () 안에는 순위를 매기기 위해 어떤 컬럼으로 정렬 할지 사용 
+	FROM 
+		TBL_BUY tb, 
+		TBL_PRODUCT tp
+	WHERE tb.pcode = tp.pcode
+	GROUP BY tb.customid, tb.pcode
+ )
+SELECT  *
+FROM customSale
+WHERE customSale.RANK = 1
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
